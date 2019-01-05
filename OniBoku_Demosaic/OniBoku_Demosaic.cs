9b28ade4149db7e03b9fb6f2b526356869ba6fab
ManlyMarco <dummyaccx@gmail.com>
2019-01-05 16:19:10 +0100

    Initial release

---
+++ OniBoku_Demosaic/OniBoku_Demosaic.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using Harmony;
+
+namespace OniBoku_Demosaic
+{
+    [BepInPlugin("OniBoku_Demosaic", "OniBoku Demosaic", "1.0")]
+    public class OniBoku_Demosaic : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            HarmonyInstance.Create("OniBoku_Demosaic").PatchAll(typeof(OniBoku_Demosaic));
+        }
+        
+        [HarmonyPatch(typeof(ChangeMozaicSize), "Start")]
+        [HarmonyPrefix]
+        public static bool MosaicDisable(ChangeMozaicSize __instance)
+        {
+            __instance.MozNum = 0;
+            __instance.enabled = false;
+            return false;
+        }
+    }
+}
---
+++ OniBoku_Demosaic/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OniBoku_Demosaic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OniBoku_Demosaic")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5929d52-9e58-4e33-affc-6e3e12582dbd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
